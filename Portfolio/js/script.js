23b3f788391d78d3719742687c6602f013211091
Marcelo <msanchez@unca.edu>
2021-05-22 12:16:14 -0400

    Fixing img paths

--- Portfolio/js/script.js
+++
@@ -1,9 +0,0 @@
-function redirectToInstagram(){
-    window.location.href = "https://instagram.com/90degreefilmz?igshid=f9eghitdgg5g";       
-}
-function redirectToYoutube(){
-    window.location.href = "https://www.youtube.com/channel/UCJbVi4Jy9Qqx0ULZkMZGSgQ";       
-}
-function redirectToFacebook(){
-    window.location.href = "https://www.facebook.com/jay.harris.1217727";       
-}
